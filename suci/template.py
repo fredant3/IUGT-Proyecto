40e89dcdae1d27487f4de8016ed768b56a49b226
Freddy <144859937+fredant3@users.noreply.github.com>
2025-09-10 22:37:33 -0400

    cambios

--- suci/template.py
+++ suci/template.py
@@ -5,18 +5,17 @@
 # Theme Variables
 # ? Personalize template by changing theme variables (For ex: Name, URL Version etc...)
 THEME_VARIABLES = {
-    "creator_name": "UNEXCA - I30142",
+    "creator_name": "Freddy Moreno para el IUGT Ven911",
     "template_name": "SUCI",
     "template_suffix": "Ven911",
     "template_version": "1.0.0",
     "template_free": True,
-    "template_description": "Aplicacion interna para el Ven911.",
+    "template_description": "Aplicacion interna para la administracion del Ven911.",
     "template_keyword": "SUCI, Ven911, 911",
-    "support": "https://github.com/erickjpl/suci-ven911/blob/main/ISSUES.md",
+    "support": "https://github.com/fredant3/IUGT-Proyecto.git/blob/main/ISSUES.md",
     "documentation": "https://demos.themeselection.com/sneat-bootstrap-html-admin-template/documentation/django-introduction.html",
-    "changelog": "https://github.com/erickjpl/suci-ven911/blob/main/CHANGELOG.md",
-    "git_repository": "https://github.com/erickjpl/suci-ven911",
-    "license_url": "https://github.com/erickjpl/suci-ven911/blob/main/README.md",
+    "changelog": "https://github.com/fredant3/IUGT-Proyecto.git/blob/main/CHANGELOG.md",
+    "license_url": "https://github.com/fredant3/IUGT-Proyecto.git/blob/main/README.md",
     "breadcrumbs_dashboard_modules_index": "SUCI",
 }
 
